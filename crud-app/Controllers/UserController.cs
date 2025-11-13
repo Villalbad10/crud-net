035528558d4be33b60ffcd715981e60f6e2cfb91
Diego Villalba <diego.villalba01@uptc.edu.co>
2025-11-13 17:43:24 -0500

    primeras rutas

---
+++ crud-app/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace crud_app.Controllers
+{
+    [ApiController]
+    [Route("user")]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        [Route("list")]
+        public dynamic listarUser() { 
+            return new { message = "List of users" };
+        }
+
+        [HttpPost]
+        [Route("save")]
+        public dynamic guardarUser(User user) {
+            
+            user.
+            return new { message = "User saved" };
+        }
+    }
+}
